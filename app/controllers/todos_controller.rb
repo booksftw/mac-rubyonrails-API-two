d7fcffbef9228971b73331d096a91527eb2a6f29
Nicholas Zachary <nicholas.zachary@outlook.com>
2023-01-27 16:42:47 -0800

    update schema

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -46,6 +46,6 @@ class TodosController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def todo_params
-      params.require(:todo).permit(:content, :currentInteger)
+      params.require(:todo).permit(:content, :currentIndex, :currentInteger)
     end
 end
